0a676911f57f9329661e6695665f9cb6817174ac
galirkil <galirkil@gmail.com>
2023-12-03 17:03:19 +0300

    Add SbisContactsPage class with some methods
    Add SbisContacsPage class with click_on_tenzor_banner, change_region,
    should_be_expected_slug_in_url, should_be_expected_title,
    should_be_expected_region_name methods.

--- pages/sbis_pages.py
+++ pages/sbis_pages.py
@@ -1,5 +1,8 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
 from pages.base_page import BasePage
-from pages.locators import SbisMainPageLocators
+from pages.locators import SbisContacsPageLocators, SbisMainPageLocators
 
 
 class SbisMainPage(BasePage):
@@ -12,3 +15,43 @@ class SbisMainPage(BasePage):
         self.driver.find_element(
             *SbisMainPageLocators.CLOSE_COOKIE_AGREEMENT_NOTIFICATION
         ).click()
+
+class SbisContactsPage(BasePage):
+    def click_on_tenzor_banner(self):
+        self.driver.find_element(
+            *SbisContacsPageLocators.TENZOR_BANNER
+        ).click()
+
+    def change_region(self, region_locator: tuple):
+        self.driver.find_element(
+            *SbisContacsPageLocators.CHANGE_REGION_BUTTON
+        ).click()
+        self.driver.find_element(
+            *region_locator
+        ).click()
+
+    def should_be_expected_slug_in_url(self, slug: str):
+        WebDriverWait(self.driver, 7).until(
+            ec.url_changes(self.driver.current_url)
+        )
+        url = self.driver.current_url
+        assert slug in url, (
+            f'Wrong url!'
+            f'Expected {slug!r} in {url!r}'
+        )
+
+    def should_be_expected_title(self, expected_string: str):
+        title = self.driver.title
+        assert expected_string in title, (
+            f'Wrong title!'
+            f'Expected {expected_string!r} in {title!r}'
+        )
+
+    def should_be_expected_region_name(self, expected_name: str):
+        name = self.driver.find_element(
+            *SbisContacsPageLocators.CHANGE_REGION_BUTTON
+        ).text
+        assert name == expected_name, (
+            f'Wrong region name!'
+            f'Expected {expected_name!r}, got {name!r}'
+        )
