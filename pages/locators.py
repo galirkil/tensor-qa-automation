404d5716bb4302220f87e7d1d9e6fd3401f1e85e
galirkil <galirkil@gmail.com>
2023-12-03 18:20:53 +0300

    Add new locators
    New locators for SbisContacsPage, SbisDownloadPage, TenzorMainPage,
    TenzorAboutPage

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,57 @@ class SbisMainPageLocators:
         By.CSS_SELECTOR,
         '.sbis_ru-CookieAgreement__close'
     )
+
+
+class SbisContacsPageLocators:
+    TENZOR_BANNER = (By.CSS_SELECTOR, '#contacts_clients a[title="tensor.ru"]')
+    CHANGE_REGION_BUTTON = (
+        By.CSS_SELECTOR,
+        '.sbisru-Contacts__relative .sbis_ru-Region-Chooser__text'
+    )
+    KAMCHATKA_SELECTOR = (
+        By.CSS_SELECTOR,
+        '[title="Камчатский край"]'
+    )
+    TATARSTAN_REGION_INFO = {
+        'name': 'Республика Татарстан',
+        'slug': '16-respublika-tatarstan',
+    }
+    KAMCHATKA_REGION_INFO = {
+        'name': 'Камчатский край',
+        'slug': '41-kamchatskij-kraj',
+    }
+    PARTNERS_LIST = (
+        By.CSS_SELECTOR,
+        '[data-qa="items-container"]'
+    )
+
+
+class SbisDownloadPageLocators:
+    SBIS_PLUGIN_MENU_ITEM = (
+        By.CSS_SELECTOR,
+        '[data-id="plugin"] .controls-tabButton__overlay'
+    )
+    DOWNLOAD_WEB_INSTALLER_LINK = (
+        By.XPATH,
+        '//a[contains(text(), "Exe")]'
+    )
+
+
+class TenzorMainPageLocators:
+    CLOSE_COOKIE_AGREEMENT_NOTIFICATION = (
+        By.CLASS_NAME,
+        'tensor_ru-CookieAgreement__close'
+    )
+    SILA_V_LYUDYAH_BLOCK = (
+        By.XPATH,
+        '//div[@class="tensor_ru-Index__block4-content tensor_ru-Index__card"]'
+        '/p[text()="Сила в людях"]'
+    )
+    SILA_V_LYUDYAH_BLOCK_PODROBNEE_ = (
+        By.CSS_SELECTOR, '.tensor_ru-Index__block4-content a'
+    )
+
+
+class TenzorAboutPageLocators:
+    RABOTAEM_BLOCK_IMAGES = (By.CLASS_NAME, 'tensor_ru-About__block3-image')
