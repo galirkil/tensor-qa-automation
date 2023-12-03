d097a45e296452269a14409faf691c936e32c028
galirkil <galirkil@gmail.com>
2023-12-03 20:17:50 +0300

    Add driver conf: download files to pwd

--- conftest.py
+++ conftest.py
@@ -1,6 +1,10 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
@@ -12,7 +16,11 @@ def driver(request):
     browser_name = request.config.getoption('browser_name')
     if browser_name == 'chrome':
         print('\nstart browser for test..')
-        driver = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'download.default_directory': BASE_DIR}
+        )
+        driver = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
         print('\nstart browser for test..')
         driver = webdriver.Firefox()
