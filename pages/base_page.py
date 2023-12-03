82cf047d9af11d810e8e790e9f89981c1c1874dc
galirkil <galirkil@gmail.com>
2023-12-03 15:41:35 +0300

    Add is_element_present method to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, driver, url, timeout=10):
         self.driver = driver
@@ -6,3 +9,10 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
