dab44ae8cf47d0530c1761fff1e9abe02015321b
galirkil <galirkil@gmail.com>
2023-12-03 15:59:40 +0300

    Add switch_to_new_tab method to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class BasePage:
@@ -9,6 +11,19 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def switch_to_new_tab(self):
+        """
+        Switch focus to new opened tab or window.
+        Works only for two opened tabs/windows.
+        """
+        current_tab = self.driver.current_window_handle
+        WebDriverWait(self.driver, 10).until(ec.number_of_windows_to_be(2))
+
+        for tab in self.driver.window_handles:
+            if tab != current_tab:
+                self.driver.switch_to.window(tab)
+                break
 
     def is_element_present(self, how, what):
         try:
